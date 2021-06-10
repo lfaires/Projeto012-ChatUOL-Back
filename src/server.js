93098e167e2c3624873dbe4f8b789fbd53350e15
Luiz Fernando <lfaires@gmail.com>
2021-06-10 18:57:26 -0300

    post method to /participants route

--- src/server.js
+++ src/server.js
@@ -1,8 +1,28 @@
-import express, { json } from 'express'
+import express from 'express'
 import cors from 'cors'
+import fs from 'fs'
+import dayjs from 'dayjs'
 
 const app = express()
 
-app.use(express(JSON))
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+const users = [];
+
+app.post('/participants', (req,res) => {
+    const { name } = req.body
+
+    if (!name){
+        res.status(404)
+        return
+    }
+    const user = {name: name, lastStatus: Date.now()}
+    users.push(user)
+    const msg = {from: name,to: "Todos", text: 'entra na sala...', type: 'status', time: dayjs(Date.now()).format('HH:mm:ss')}
+    res.status(200)
+})
+
+app.listen(4000, ()=>{
+    console.log('Server running on port 4000')
+})
