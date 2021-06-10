7fc716fdc86009057c9ccd73cc6ed6d2c28fddfe
Luiz Fernando <lfaires@gmail.com>
2021-06-10 19:38:15 -0300

    post method to /messages route

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 const users = [];
+const messages = [];
 
 app.post('/participants', (req,res) => {
     const { name } = req.body
@@ -19,12 +20,30 @@ app.post('/participants', (req,res) => {
     }
     const user = {name: name, lastStatus: Date.now()}
     users.push(user)
-    const msg = {from: name,to: "Todos", text: 'entra na sala...', type: 'status', time: dayjs(Date.now()).format('HH:mm:ss')}
+    const message = {from: name,to: "Todos", text: 'entra na sala...', type: 'status', time: dayjs(Date.now()).format('HH:mm:ss')}
+    messages.push(message)
     res.status(200)
 })
 
 app.get('/participants', (req,res) => {
     res.send(users)
+})
+
+app.post('/messages', (req,res) => {
+    const {to, text, type} = req.body;
+    const from = req.header('User')
+    const notEmptyString = to && text
+    const typeOfMessage = (type === 'private_message') || (type === 'message' )
+    const alreadyAUser = users.find( user => from === user.name)
+
+    if(notEmptyString && typeOfMessage && alreadyAUser){
+        res.status(400)
+        return
+    }
+
+    const message = {from, to, text, type, time: dayjs(Date.now()).format('HH:mm:ss')}
+    messages.push(message)
+    res.status(200)
 })
 
 app.listen(4000, ()=>{
